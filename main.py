be3aa4a985654ebce20d10dce8f6d9378e03941b
Wei Yang Zhu <weiweizhu0304@gmail.com>
2024-11-18 16:21:54 -0500

    from to where

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import pygame
-from src import sample_controller
+from src.sample_controller import Controller
 #import your controller
 
 def main():
     pygame.init()
     #Create an instance on your controller object
-    game = sample_controller.Controller(1000, 1000, 60)
+    game = Controller(1000, 1000, 60)
     #Call your mainloop
     game.mainloop()
     ###### NOTHING ELSE SHOULD GO IN main(), JUST THE ABOVE 3 LINES OF CODE ######
